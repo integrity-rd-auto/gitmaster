17ca19f8dfd6df438ffae6f754f19db7bfb6bbf6
integrity-rd-auto <subh078@yahoo.in>
2020-01-11 20:00:58 +0530

    Created Maven Project

---
+++ com.ptc.git/src/main/java/com/ptc/git/com/ptc/git/App.java
@@ -0,0 +1,13 @@
+package com.ptc.git.com.ptc.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
